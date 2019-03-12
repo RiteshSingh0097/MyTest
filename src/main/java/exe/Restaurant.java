f6f6c5a1826af238f406b974a6e862349154770a
RiteshSingh0097 <ritesh893@outlook.com>
2019-03-12 21:27:22 +0530

    modified

--- src/main/java/exe/Restaurant.java
+++ src/main/java/exe/Restaurant.java
@@ -1,6 +1,5 @@
 package exe;
 
-import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
@@ -8,6 +7,7 @@ import org.springframework.stereotype.Component;
 interface HotDrink {
     void preparedHotDrink();
 }
+
 @Component("tea")
 class Tea implements HotDrink {
     @Override
@@ -15,6 +15,7 @@ class Tea implements HotDrink {
         System.out.println("Preparing hot tea....");
     }
 }
+
 @Component("expresstea")
 class ExpressTea implements HotDrink {
     @Override
@@ -49,6 +50,7 @@ public class Restaurant {
     public void setHotDrink(HotDrink hotDrink) {
         this.hotDrink = hotDrink;
     }
+
     @Autowired
     public HotDrink getHotDrink() {
         return hotDrink;
